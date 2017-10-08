bde9216ecf34b1eec93a63509530eceb8d0e8a25
Thomas Jaeckle <thjaeckle@gmail.com>
2017-10-08 17:08:02 +0200

    changed db.js migration script
    Signed-off-by: Thomas Jaeckle <thjaeckle@gmail.com>

--- db.js
+++ db.js
@@ -1,7 +1,28 @@
+//var KnexMigrator = require('knex-migrator');
+//var knexMigrator = new KnexMigrator({
+//	    knexMigratorFilePath: __dirname
+//});
+
+//knexMigrator.init();
+// knexMigrator.migrate();
+
 var KnexMigrator = require('knex-migrator');
 var knexMigrator = new KnexMigrator({
-	    knexMigratorFilePath: __dirname
+    knexMigratorFilePath: __dirname
 });
 
-knexMigrator.init();
-// knexMigrator.migrate();
+// check your database health
+knexMigrator.isDatabaseOK()
+  .then(function() {
+     // database is OK
+  })
+  .catch(function(err) {
+     // err contains a specific code, based on that code you decide (err.code)
+     
+     // database is not initialised?
+     knexMigrator.init();
+     
+     // database is not migrated?
+     knexMigrator.migrate();
+  });
+
